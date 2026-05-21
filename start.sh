6ff6cab4cd7a0debc13582f742f38dbadac2ea88
awmgEw <awmgew@github.com>
2026-05-21 16:53:31 +0300

    Update start.sh to run only the server

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/data/data/com.termux/files/usr/bin/bash
 cd ~/ship-logger
-echo "Запуск Бортового Журнала..."
-am start -a android.intent.action.VIEW -d "http://localhost:8765" com.android.chrome &
+echo "Запуск сервера Бортового Журнала на http://localhost:8765"
 python3 -m http.server 8765
